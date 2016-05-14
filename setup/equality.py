a3d8256f3fb2eab081c6a84b3bf27c9c1bde98f9
Aubhro <aubrosengupta@gmail.com>
2016-05-14 15:27:31 -0700

    Marked equality.py as deprecated
    Many of the classes have their own .equals() methods, which makes a
    separate class for it obsolete

--- setup/equality.py
+++ setup/equality.py
@@ -1,12 +1,28 @@
 # TODO old code delete when fully phased out
-
+import warnings
 """
 Class that contains all equality comparison methods specific for chess_py
 """
 
 from setup.algebraic_notation import algebraic
 
+def deprecated(func):
+    """This is a decorator which can be used to mark functions
+    as deprecated. It will result in a warning being emmitted
+    when the function is used."""
 
+    def new_func(*args, **kwargs):
+        warnings.simplefilter('always', DeprecationWarning) #turn off filter
+        warnings.warn("Call to deprecated function {}.".format(func.__name__), category=DeprecationWarning, stacklevel=2)
+        warnings.simplefilter('default', DeprecationWarning) #reset filter
+        return func(*args, **kwargs)
+
+    new_func.__name__ = func.__name__
+    new_func.__doc__ = func.__doc__
+    new_func.__dict__.update(func.__dict__)
+    return new_func
+
+@deprecated
 def location_not_none(location):
 
     """
@@ -16,6 +32,7 @@ def location_not_none(location):
     """
     return location is not None and location.rank is not None and location.file is not None
 
+@deprecated
 def move_not_none(move):
 
     """
@@ -25,6 +42,7 @@ def move_not_none(move):
     """
     return move is not None and move.rank is not None and move.file is not None and move.piece is not None
 
+@deprecated
 def piece_matches_description(piece, reference_type, reference_color):
 
     """
@@ -36,6 +54,7 @@ def piece_matches_description(piece, reference_type, reference_color):
     """
     return type(piece) is reference_type and piece.white == reference_color is "white"
 
+@deprecated
 def piece_equals(piece1, piece2):
 
     """
@@ -46,6 +65,7 @@ def piece_equals(piece1, piece2):
     """
     return type(piece1) == type(piece2) and piece1.white == piece2.white
 
+@deprecated
 def find_piece(piece, position):
 
     """
