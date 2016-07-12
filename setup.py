ae6112c7af276a88eb7ee675dce1ba2580b3795c
Aubhro <aubhrosengupta@gmail.com>
2016-07-12 16:35:48 -0700

    Updated classifiers in setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='chess_py',
-      version='1.0',
+      version='1.2',
       description='Python chess client',
+      platforms='MacOS X',
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3.5',
+          'Operating System :: MacOS :: MacOS X',
+          'Operating System :: Microsoft :: Windows',
+      ],
       author='Aubhro Sengupta',
       author_email='aubhrosengupta@gmail.com',
       url='https://github.com/LordDarkula/chess_py',
+      license='MIT',
       packages=['chess_py', 'chess_py.core', 'chess_py.core.algebraic', 'chess_py.game', 'chess_py.pieces',
                 'chess_py.players'],
       )
