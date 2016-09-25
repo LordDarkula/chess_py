67ad88abf09f951376705cbb0b50a0efe8393f29
Aubhro <aubhrosengupta@gmail.com>
2016-09-25 12:00:41 -0700

    Refresh

--- chess_py/game/__init__.py
+++
@@ -1,4 +0,0 @@
-from chess_py.game.game import Game
-from chess_py.game.game_state import no_moves, is_checkmate
-
-__all__ = ['Game', 'no_moves', 'is_checkmate']
--- chess_py/game/interface.py
+++
@@ -1,9 +0,0 @@
-
-
-class UCI:
-    def __init__(self, player):
-        """
-
-        :type player Player
-        """
-        # TODO finish writing UCI commands
