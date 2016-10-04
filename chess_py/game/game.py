dbb7163f29cc0ed9f3e0cffed9e4fb46801083d1
Aubhro Sengupta <aubhrosengupta@gmail.com>
2016-10-04 12:29:28 -0700

    Update Game docstrings

--- chess_py/game/game.py
+++ chess_py/game/game.py
@@ -2,7 +2,15 @@
 
 """
 Class that holds state of a game.
-Start game using play(), which returns the result
+
+Takes two subclasses of Player as defined
+in chess_py.players.player.Player, and calls 
+<code>generate_move(position)</code>
+from each of the players and updates the board using 
+each corresponding result.
+
+Start game using play(), which returns the result 
+(1 - white wins, 0 - black wins, 0.5 - draw)
 when the game is finished.
 
 Copyright © 2016 Aubhro Sengupta. All rights reserved.
@@ -29,11 +37,11 @@ class Game:
 
     def play(self):
         """
-        Starts game and returns one of 3 results . .
-
-        1 - White wins
-        0.5 - Draw
-        0 - Black wins
+        Starts game and returns one of 3 results . 
+        Iterates between methods <code>white_move()</code> and
+        <code>black_move()</code> until game ends. Each
+        method calls the respective player's <code>generate_move()</code>
+        method.
 
         :rtype: int
         """
@@ -55,14 +63,28 @@ class Game:
             color_fn()
 
     def white_move(self):
+        """
+        Calls the white player's <code>generate_move()</code>
+        method and updates the board with the move returned.
+        """
         move = self.player_white.generate_move(self.position)
         move = make_legal(move, self.position)
         self.position.update(move)
 
     def black_move(self):
+        """
+        Calls the black player's <code>generate_move()</code>
+        method and updates the board with the move returned.
+        """
         move = self.player_black.generate_move(self.position)
         move = make_legal(move, self.position)
         self.position.update(move)
 
     def all_possible_moves(self, input_color):
+        """
+        Finds all possible moves a particular player can
+        play during a game. Calling this method is recommended over
+        calling the <code>all_possible_moves(input_color)</code>
+        from this <code>Board</code> directly.
+        """
         return self.position.all_possible_moves(input_color)
