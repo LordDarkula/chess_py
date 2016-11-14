dfc8c7c432528d66d9b87450872a0cf62a0ae335
Aubhro <aubhrosengupta@gmail.com>
2016-11-13 18:45:22 -0800

    Switch to pytest in CI

--- chess_py/__init__.py
+++ chess_py/__init__.py
@@ -2,3 +2,5 @@ from .core import *
 from .game import *
 from .pieces import *
 from .players import *
+
+__all__ = core.__all__ + game.__all__ + pieces.__all__ + players.__all__
--- chess_py/core/__init__.py
+++ chess_py/core/__init__.py
@@ -2,4 +2,4 @@ from . import algebraic, color
 from .algebraic import Move, Location, converter, notation_const
 from .board import Board
 
-__all__ = (['Board', 'color'] + algebraic.__all__)
+__all__ = ['Board', 'color'] + algebraic.__all__
