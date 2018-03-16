fd84cfb58f75b64dc5d8e38f1d73316d250688b9
Aubhro Sengupta <aubhrosengupta@gmail.com>
2018-03-15 19:01:17 -0700

    Update version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", 'r') as f:
 
 setup(
     name='chess_py',
-    version='2.7.6',
+    version='2.7.7',
     description='Python chess client',
     long_description=long_description,
     platforms='MacOS X, Windows, Linux',
