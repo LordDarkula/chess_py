b3d259965a6f810760c706ef0f6638e159204888
Aubhro Sengupta <aubhrosengupta@gmail.com>
2018-03-22 22:08:32 -0700

    Move en passant detection clause
    En passant clause never triggered

--- chess_py/core/algebraic/converter.py
+++ chess_py/core/algebraic/converter.py
@@ -169,15 +169,17 @@ def incomplete_alg(alg_str, input_color, position):
                 possible_pawn = position.piece_at_square(pawn_location)
                 if type(possible_pawn) is Pawn and \
                         possible_pawn.color == input_color:
-                    return Move(end_loc=end_location,
-                                piece=position.piece_at_square(pawn_location),
-                                status=notation_const.CAPTURE,
-                                start_loc=pawn_location)
-                else:
-                    return Move(end_loc=end_location,
-                                piece=position.piece_at_square(pawn_location),
-                                status=notation_const.EN_PASSANT,
-                                start_loc=pawn_location)
+                    enemy_pawn = position.piece_at_square(end_location)
+                    if type(enemy_pawn) is Pawn and enemy_pawn.color != input_color:
+                        return Move(end_loc=end_location,
+                                    piece=position.piece_at_square(pawn_location),
+                                    status=notation_const.CAPTURE,
+                                    start_loc=pawn_location)
+                    else:
+                        return Move(end_loc=end_location,
+                                    piece=position.piece_at_square(pawn_location),
+                                    status=notation_const.EN_PASSANT,
+                                    start_loc=pawn_location)
 
             # Piece capture
             elif alg_str[0].isupper():
